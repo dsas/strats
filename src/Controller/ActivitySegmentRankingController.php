0ae1aa4891533f35f8cef3db8b5bb1d065723a73
Dean Sas <dean@deansas.org>
2020-02-24 21:57:01 +0000

    ActivitySegmentRanker had a missing use
    Added the symfony AbstractController

--- src/Controller/ActivitySegmentRankingController.php
+++ src/Controller/ActivitySegmentRankingController.php
@@ -6,6 +6,7 @@ use Iamstuartwilson\StravaApi;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ActivitySegmentRankingController extends AbstractController
 {
